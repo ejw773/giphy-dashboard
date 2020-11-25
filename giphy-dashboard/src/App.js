62c5e8c1289b70065bbd3a29a8cf834b9d156dd4
Elijah Wilcott <ejw773@gmail.com>
2020-11-25 12:51:52 -0500

    style changes

--- giphy-dashboard/src/App.js
+++ giphy-dashboard/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GifCard from './GifCard'
+import './App.css'
 
 class App extends React.Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class App extends React.Component {
         fetch('https://api.giphy.com/v1/gifs/trending?api_key=i8NdfYxYVasb7W5dfgkdgpWGBJ6e5WAA&limit=25&rating=g')
             .then(res => res.json())
             .then(data => {
-                console.log(data.data)
+                console.log(data.data[0])
             })
     }
 
@@ -29,9 +30,9 @@ class App extends React.Component {
         return (
             <div>
                 <div>
-                    <h1 style={{textAlign: 'center'}}>Trending on Giphy.com</h1>
+                    <h1>Trending on Giphy.com</h1>
                 </div>
-                <div className='gify-container'>
+                <div className='giphy-container'>
                     <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
                     <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
                     <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
