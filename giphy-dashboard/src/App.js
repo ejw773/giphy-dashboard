82c35861354872299167e786fb302f4ee2a34295
ejw773 <ejw773@gmail.com>
2020-11-25 04:44:56 -0500

    adds bootstrap

--- giphy-dashboard/src/App.js
+++ giphy-dashboard/src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import GifCard from './GifCard'
 
-class ApiCall extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount() {
+        console.log("Here I am.");
+        fetch('https://api.giphy.com/v1/gifs/trending?api_key=i8NdfYxYVasb7W5dfgkdgpWGBJ6e5WAA&limit=25&rating=g')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.data)
+            })
+    }
+
+    componentDidUpdate() {
+        console.log("I updated.")
+    }
+
+    componentWillUnmount() {
+        console.log("Bye,bye")
+    }
+
+
+
     render() {
         return (
             <div>
-                <h1>Bring Out Your Dead!</h1>
+                <div>
+                    <h1 style={{textAlign: 'center'}}>Trending on Giphy.com</h1>
+                </div>
+                <div className='gify-container'>
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                    <GifCard url="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" />
+                </div>
             </div>
         )
     }
 }
 
-export default ApiCall;
+export default App;
--- giphy-dashboard/src/GifCard.js
+++ giphy-dashboard/src/GifCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class GifCard extends React.Component {
+    constructor(props) {
+        super(props)
+
+        // this.state = {
+        //     gifPick: [],
+        //     isReady: false,
+        //     hideIt: false,
+        //     likeIt: false
+        // }
+    }
+    
     render() {
         return (
             <div className="card" style={{width: "18rem"}}>
-              <img src="https://media1.giphy.com/media/3osxYrgM8gi9CDjcPu/giphy.gif?cid=df1535562701hkitcnuu9qkj5n9rfyljuzvkq61hr8fjpo9w&rid=giphy.gif" className="card-img-top" alt="missing" />
+              <img src={this.props.url} className="card-img-top" alt="missing" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
